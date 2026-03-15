1a587e5d515ee36ebccb9417e4ff8cbfc50e8cb7
Sumit Akhuli <sumitakhuli5022@gmail.com>
2026-03-15 09:15:15 +0530

    Use of warning instead of logging

--- quizcomp/converter/html.py
+++ quizcomp/converter/html.py
@@ -2,7 +2,7 @@
 Convert a quiz into HTML using templates.
 """
 
-import logging
+import warnings
 import os
 
 import quizcomp.constants
@@ -13,7 +13,6 @@ DEFAULT_TEMPLATE_DIR = os.path.join(THIS_DIR, '..', 'data', 'templates', 'edq-ht
 
 CSS_FILENAME = 'quiz.css'
 
-logger = logging.getLogger(__name__)
 
 class HTMLTemplateConverter(quizcomp.converter.template.TemplateConverter):
     def __init__(self,
@@ -23,7 +22,7 @@ class HTMLTemplateConverter(quizcomp.converter.template.TemplateConverter):
 
         css_path = os.path.join(template_dir, CSS_FILENAME)
         if (not os.path.isfile(css_path)):
-            logger.warning("Could not find CSS file: '%s'. Continuing without external CSS.", css_path)
+            warnings.warn(f"Could not find CSS file: '{css_path}'. Continuing without external CSS.")
             self.env.globals['css_content'] = ''
         else:
             with open(css_path, 'r', encoding = 'utf-8') as css_file:
