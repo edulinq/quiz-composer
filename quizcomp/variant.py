5d9abbaec39841e4df1b883b1b59d3ed12591363
Eriq Augustine <eriq.augustine@gmail.com>
2026-03-15 14:59:17 -0700

    (wip) Working on Python typing. [skip ci]

--- quizcomp/variant.py
+++ quizcomp/variant.py
@@ -1,7 +1,10 @@
+import typing
+
 import quizcomp.common
+import quizcomp.question.base
 import quizcomp.quiz
 
-DUMMY_QUIZ_DATA = {
+DUMMY_QUIZ_DATA: typing.Dict[str, typing.Any] = {
     'title': 'Dummy Title',
     'description': 'Dummy description.',
     'course_title': 'Dummy Course',
@@ -9,7 +12,7 @@ DUMMY_QUIZ_DATA = {
     'version': '0.0.0',
 }
 
-DUMMY_GROUP_DATA = {
+DUMMY_GROUP_DATA: typing.Dict[str, typing.Any] = {
     'name': 'Dummy Question',
     'hints': {},
     'hints_first': {},
@@ -27,8 +30,9 @@ class Variant(quizcomp.quiz.Quiz):
     Quizzes created from files will undergo full validation.
     """
 
-    def __init__(self, type = quizcomp.constants.TYPE_VARIANT,
-            **kwargs):
+    def __init__(self,
+            type = quizcomp.constants.TYPE_VARIANT,
+            **kwargs: typing.Any) -> None:
         super().__init__(type = type, **kwargs)
         self.validate(cls = Variant, **kwargs)
 
@@ -36,16 +40,19 @@ class Variant(quizcomp.quiz.Quiz):
         for group in self.groups:
             self.questions += group.questions
 
-    def _validate(self, **kwargs):
+    def _validate(self, **kwargs: typing.Any) -> None:
+        """ Check if this variant is valid, will raise if the group is not valid. """
+
         # Ensure that each group has the correct number of questions.
         for i in range(len(self.groups)):
             group = self.groups[i]
 
             if (len(group.questions) != group.pick_count):
-                raise quizcomp.common.QuizValidationError("Group at index %d (%s) has %d questions, expecting exactly %d." % (i, group.name, len(group.questions), group.pick_count))
+                raise quizcomp.common.QuizValidationError(
+                        "Group at index {i} ('{group.name}') has {len(group.questions)} questions, expecting exactly {group.pick_count}.")
 
     @staticmethod
-    def get_dummy(question):
+    def get_dummy(question: quizcomp.question.base.Question) -> 'Variant':
         """
         Get a "dummy" variant that has no real information.
         """
